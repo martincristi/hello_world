f6b93c51447e9737cae06a4e8b4c28d33e4dec2a
martincristi <martin_cristi30@yahoo.com>
2020-10-16 16:23:52 +0300

    first commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val textView: TextView = findViewById(R.id.text_view)
+            textView.text = "Hello World!"
+        }
+    }
+}
